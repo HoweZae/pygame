beae1431b52917d2e98f286b7d41f0b6fe48a1d3
Jose <93581038+HoweZae@users.noreply.github.com>
2023-02-01 00:45:34 +0800

    transparent bg

--- sheeper/main.py
+++ sheeper/main.py
@@ -7,27 +7,18 @@ pygame.init()
 CLOCK = pygame.time.Clock()
 
 # GAME SCREEN
-WINDOW = pygame.display.set_mode((640, 480))
+WINDOW = pygame.display.set_mode((640, 480), pygame.HWSURFACE | pygame.DOUBLEBUF)
 screen = pygame.Surface((320, 240))
 pygame.mouse.set_visible(False)
 
 def main():
-    # BACKGROUND
+    # BACKGROUND INITIALIZATION
     grass = pygame.image.load("sheeper\images\grass.png")
-    for x in range(0, 320, 32):
-        for y in range(0, 240, 32):
-            screen.blit(grass, (x, y))
 
     # SHEEP INITIALIZATION
     sheep_group = pygame.sprite.Group()
-    memo = set([])
-    iter, count = 0, 8
-    while iter < count:
-        pos = (random.randint(32, 320 - 32), random.randint(32, 240 - 32))
-        if pos not in memo:
-            sheep_group.add(Sheep(pos))
-            memo.add(pos)
-            iter += 1
+    for i in range(8):
+        sheep_group.add(Sheep((random.randrange(1,16-1)*20, random.randrange(1,15-1)*16)))
 
     # CROSSHAIR INITIALIZATION
     xhair = Crosshair()
@@ -39,12 +30,18 @@ def main():
                 pygame.quit()
                 sys.exit()
 
+        # EVERYTHING DISPLAY RELATED
+        for x in range(0, 324, 81):
+            for y in range(0, 243, 81):
+                screen.blit(grass, (x, y))
+
+        sheep_group.update()
         sheep_group.draw(screen)
 
-        WINDOW.blit(pygame.transform.scale(screen, (640, 480)), (0, 0))
+        WINDOW.blit(pygame.transform.scale_by(screen, 2), (0, 0))
 
-        xhair_group.draw(WINDOW)
         xhair_group.update()
+        xhair_group.draw(WINDOW)
 
         pygame.display.update()
         CLOCK.tick(60)
--- sheeper/script/classes.py
+++ sheeper/script/classes.py
@@ -3,11 +3,7 @@ import pygame
 class Crosshair(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        image = pygame.Surface((19,19))
-        image.blit(pygame.image.load("sheeper\images\crosshair.png"), (0,0))
-        image.set_colorkey((255, 0, 255))
-
-        self.image = pygame.transform.scale(image, (38, 38))
+        self.image = pygame.image.load("sheeper\images\crosshairfull.png").convert_alpha()
         self.rect = self.image.get_rect()
 
     def update(self):
@@ -17,15 +13,39 @@ class Crosshair(pygame.sprite.Sprite):
         pass
 
 class Sheep(pygame.sprite.Sprite):
+    state_dict = {
+        0: (0,0,20,16),
+        1: (20,0,40,16),
+        2: (0,0,20,16),
+        3: (40,0,60,16)
+    }
+    source = pygame.image.load("sheeper\images\sheep.png").convert_alpha()
+    
     def __init__(self, pos):
         super().__init__()
-        image = pygame.Surface((32,32))
-        image.blit((pygame.image.load("sheeper\images\sheep.png")), pos, (0,0,32,32))
-        image.set_colorkey((255, 0, 255))
 
-        self.image = pygame.transform.scale(image, (64, 64))
+        image = pygame.Surface((20,16), pygame.SRCALPHA)
+        image.blit(self.source, (0,0), self.state_dict[0])
+        self.image = image
+
         self.rect = self.image.get_rect()
-        self.rect.center = pos
+        self.rect.topleft = pos
+        self.state = 0
 
     def update(self):
+        if self.state < 3:
+            self.state += 1
+        else:
+            self.state = 0
+
+        self.image.blit(self.source, (0,0), self.state_dict[self.state])
+        self.rect.left += 1
+
+    def updateLeft(self):
+        pass
+
+    def scram(self):
+        pass
+
+    def eatGrass(self):
         pass
