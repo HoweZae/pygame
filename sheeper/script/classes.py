5295dd358749ecae0764fc8b7a650e870d65ab78
Jose <93581038+HoweZae@users.noreply.github.com>
2023-02-02 17:16:48 +0800

    adding Spritesheet as a superclass

--- sheeper/script/classes.py
+++ sheeper/script/classes.py
@@ -1,5 +1,19 @@
 import pygame
 
+# SUPERCLASS
+class Spritesheet(pygame.sprite.Sprite):
+    def __init__(self, imgpath, w, h, frame_dict = None):
+        super().__init__()
+        self.sheet = pygame.image.load(imgpath).convert_alpha()
+        self.frame_dict = frame_dict
+        self.size = (w,h)
+
+    def get_image(self, frame):
+        image = pygame.Surface(self.size, pygame.SRCALPHA)
+        image.blit(self.sheet, (0,0), self.frame_dict[frame])
+        return image
+
+# SPRITESHEET SUBCLASSES
 class Crosshair(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -8,26 +22,24 @@ class Crosshair(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.center = pygame.mouse.get_pos()
+
     def shoot(self):
         pass
 
-class Sheep(pygame.sprite.Sprite):
-    frames = {
-        0: (0,0,20,16),
-        1: (20,0,40,16),
-        2: (0,0,20,16),
-        3: (40,0,80,16)
-    }
-    source = pygame.image.load("sheeper\images\sheepwalk.png").convert_alpha()
-    
+class Sheep(Spritesheet):
     def __init__(self, pos):
-        super().__init__()
+        frame_dict = {
+            0: (0, 0, 20, 16),
+            1: (20, 0, 40, 16),
+            2: (0, 0, 20, 16),
+            3: (40, 0, 60, 16)
+        }
+        super().__init__("sheeper\images\sheepwalk.png", 20, 16, frame_dict)
+
         self.tick = 0
         self.frame = 0
 
-        image = pygame.Surface((20,16), pygame.SRCALPHA)
-        image.blit(self.source, (0,0), self.frames[0])
-        self.image = image
+        self.image = super().get_image(frame = 0)
 
         self.rect = self.image.get_rect()
         self.rect.topleft = pos
@@ -39,10 +51,8 @@ class Sheep(pygame.sprite.Sprite):
         self.tick += 1
 
         frame = (self.tick // 6) % 4
-        
-        image = pygame.Surface((20,16), pygame.SRCALPHA)
-        image.blit(self.source, (0,0), self.frames[frame])
-        self.image = image
+
+        self.image = super().get_image(frame)
 
         if (frame != self.frame):
             self.rect.left += 1
@@ -53,9 +63,7 @@ class Sheep(pygame.sprite.Sprite):
 
         frame = (self.tick // 6) % 4
         
-        image = pygame.Surface((20,16), pygame.SRCALPHA)
-        image.blit(self.source, (0,0), self.frames[frame])
-        self.image = pygame.transform.flip(image, True, False)
+        self.image = pygame.transform.flip(super().get_image(frame), True, False)
 
         if (frame != self.frame):
             self.rect.left -= 1
